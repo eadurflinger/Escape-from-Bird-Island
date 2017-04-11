fbddf9f92c83350cc8b4c4ff7fd4225e6bea95ee
eadurflinger <eadurflinger@gmail.com>
2017-04-10 22:46:51 -0400

    Revert "Added User Input"
    This reverts commit 1b8db8675d3649377544a422c41408e25b23b8c5.

--- printemailsofgithubusers.py
+++
@@ -1,9 +0,0 @@
-import requests
-usernames = []
-response = requests.get('https://api.github.com/search/users?q=location:youngstown&per_page=100')
-# print response.json()["items"][0]["login"]
-items = response.json()["items"]
-for item in items:
-    usernames.append(item["login"])
-
-print
